7ea0b60b756c5d307de45100c6307b431ba64382
Andre Coelho <andrecoelho03@gmail.com>
2011-12-10 14:35:46 -0300

    lojavirtual

---
+++ src/main/java/br/com/wemob/lojavirtual/controller/HelloController.java
@@ -0,0 +1,15 @@
+package br.com.wemob.lojavirtual.controller;
+
+import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Resource;
+
+@Resource
+public class HelloController {
+	
+	@Get("/hello")
+	public void hello(){
+		
+	}
+	
+
+}
